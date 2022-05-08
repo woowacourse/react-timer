5b41dce5c82c8a66c4fe157f279f4a71c9359591
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-05-08 17:27:53 +0900

    feat: 타이머 버튼 기능 구현
    details:
    수정버튼
    재설정버튼
    시작버튼

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-
+import { useState, useRef, useEffect } from 'react';
 import TimerButton from './components/common/TimerButton';
 import TimerPannel from './components/common/TimerPannel';
 
@@ -28,17 +28,75 @@ const TimerButtonBox = styled.div`
 `;
 
 const App = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const timerId = useRef();
+  const initialTime = useRef(0);
+
+  const startTimer = () => {
+    setIsRunning(true);
+    timerId.current = setInterval(() => {
+      setTime((prev) => prev - 1);
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+    setIsRunning(false);
+  };
+
+  const modifyTimer = () => {
+    const newMinute = prompt('몇 분를 셀 것인지 입력해주세요');
+    const newSecond = prompt('몇 초를 셀 것인지 입력해주세요');
+
+    const calcSecond = Number(newMinute) * 60 + Number(newSecond);
+    setTime(calcSecond);
+
+    initialTime.current = calcSecond;
+  };
+
+  const resetTimer = () => {
+    setTime(initialTime.current);
+    clearInterval(timerId.current);
+    setIsRunning(false);
+  };
+
+  const minute = String(parseInt(time / 60)).padStart(2, '0');
+  const second = String(time % 60).padStart(2, '0');
+
+  useEffect(() => {
+    if (isRunning && time === 0) {
+      alert('타이머가 종료되었습니다.');
+      clearInterval(timerId.current);
+      setIsRunning(false);
+    }
+  }, [time, isRunning]);
+
   return (
     <div>
       <Header />
       <ContentContainer>
         <NavBar />
         <ContentBox>
-          <TimerPannel>00:00</TimerPannel>
+          <TimerPannel>
+            {minute} : {second}
+          </TimerPannel>
           <TimerButtonBox>
-            <TimerButton color='lokbaRed'>타이머 수정</TimerButton>
-            <TimerButton color='smingBlue'>재설정</TimerButton>
-            <TimerButton color='binyeoGreen'>시작</TimerButton>
+            <TimerButton color='lokbaRed' onClick={modifyTimer}>
+              타이머 수정
+            </TimerButton>
+            <TimerButton color='smingBlue' onClick={resetTimer}>
+              재설정
+            </TimerButton>
+            {isRunning ? (
+              <TimerButton color='sophiaPink' onClick={stopTimer}>
+                중지
+              </TimerButton>
+            ) : (
+              <TimerButton color='binyeoGreen' onClick={startTimer}>
+                시작
+              </TimerButton>
+            )}
           </TimerButtonBox>
         </ContentBox>
       </ContentContainer>
--- src/components/common/TimerPannel.jsx
+++ src/components/common/TimerPannel.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import DigitalFont from '../../styles/font/digital-7.ttf';
 
 const TimerPanelBox = styled.div`
   width: 100%;
@@ -9,11 +8,7 @@ const TimerPanelBox = styled.div`
 `;
 
 const TimerPanelText = styled.div`
-  @font-face {
-    font-family: 'digital-7';
-    src: url(${DigitalFont});
-  }
-  font-family: 'digital-7';
+  font-family: 'digital';
   font-weight: 320;
   font-size: 140px;
   color: ${({ theme }) => theme.colors.clock};
