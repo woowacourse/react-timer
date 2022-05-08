d1ca7d2104f1d7e559c23497179b5cc00dab55db
Soyi Jeon <soyi.dev@gmail.com>
2022-05-08 22:14:44 +0900

    test: storybook 전역 스타일 추가
    Co-authored-by: Jeongmin Woo <greenblues1190@users.noreply.github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../src/GlobalStyle";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +9,21 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+const theme = {
+  woowagreen: "#6FA189",
+  woowawhite: "#FFFFFF",
+  woowablack: "#000000",
+};
+
+export const decorators = [
+  (Story, context) => {
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story {...context} />
+      </ThemeProvider>
+    );
+  },
+];
