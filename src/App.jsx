f528bedbddc7243327d3718f29b14eaebcf318bd
Soyi Jeon <soyi.dev@gmail.com>
2022-05-25 14:28:14 +0900

    style: StyledComponent 위치 이동

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,36 @@ import NavLink from "./components/NavLink/NavLink";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <StyledLayout>
+        <header className="left-top-panel">
+          <Logo />
+        </header>
+        <NavBar className="left-middle-panel">
+          <NavLink to="/">카운트다운</NavLink>
+          <NavLink to="/">스톱워치</NavLink>
+          <NavLink to="/">세계시계</NavLink>
+        </NavBar>
+        <main className="right-panel">
+          <Routes>
+            <Route path="/" element={<CountDownPage />} />
+          </Routes>
+        </main>
+        <footer className="left-bottom-panel">
+          <CorpAddress>
+            <p>사업자: 박공원</p>
+            <p>사업자등록번호: 220208</p>
+            <p>주소: 서울특별시 송파구 루터회관 14층 페어룸4</p>
+          </CorpAddress>
+        </footer>
+      </StyledLayout>
+    </ThemeProvider>
+  );
+}
+
 const StyledLayout = styled.div`
   height: 100vh;
 
@@ -45,35 +75,5 @@ const StyledLayout = styled.div`
     grid-row: 1 / 7;
   }
 `;
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <StyledLayout>
-        <header className="left-top-panel">
-          <Logo />
-        </header>
-        <NavBar className="left-middle-panel">
-          <NavLink to="/">카운트다운</NavLink>
-          <NavLink to="/">스톱워치</NavLink>
-          <NavLink to="/">세계시계</NavLink>
-        </NavBar>
-        <main className="right-panel">
-          <Routes>
-            <Route path="/" element={<CountDownPage />} />
-          </Routes>
-        </main>
-        <footer className="left-bottom-panel">
-          <CorpAddress>
-            <p>사업자: 박공원</p>
-            <p>사업자등록번호: 220208</p>
-            <p>주소: 서울특별시 송파구 루터회관 14층 페어룸4</p>
-          </CorpAddress>
-        </footer>
-      </StyledLayout>
-    </ThemeProvider>
-  );
-}
 
 export default App;
