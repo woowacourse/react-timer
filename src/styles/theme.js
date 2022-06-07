866583cd25a2e2c77b4e9d26263c04a1bb4260f5
공원 <shinyoung.park@woowahan.com>
2022-06-07 12:57:09 +0900

    Revert "[온보딩-타이머] 우디 & 소피아 미션 제출합니다"

--- src/styles/theme.js
+++
@@ -1,9 +0,0 @@
-const theme = {
-  placeholderColor: "#a5a5a5",
-  woowagreen: "#6FA189",
-  woowalightgreen: "#95DAC5",
-  woowawhite: "#FFFFFF",
-  woowablack: "#000000",
-};
-
-export default theme;
