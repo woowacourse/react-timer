427fc9e1190245e45c856c5362bbd9fff9bd942f
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-03 18:25:13 +0900

    feat: NavIcon 컴포넌트 구현
    Co-authored-by: Nangkyeong Lim <nangkyeonglim@users.noreply.github.com>

---
+++ src/components/NavIcon.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { ReactComponent as Global } from '../assets/icons/global.svg';
+import { ReactComponent as StopWatch } from '../assets/icons/stopwatch.svg';
+import { ReactComponent as Timer } from '../assets/icons/timer.svg';
+
+const NAV_ICONS = {
+  세계시계: <Global />,
+  스톱워치: <StopWatch />,
+  타이머: <Timer />,
+};
+
+type NavIconProps = {
+  title: keyof typeof NAV_ICONS;
+};
+
+const NavIcon = ({ title }: NavIconProps) => {
+  return (
+    <Container>
+      {NAV_ICONS[title]}
+      <Title>{title}</Title>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.span`
+  font-size: 15px;
+  color: #fff;
+`;
+
+export default NavIcon;
