cb799a9fdd1557e68aeb6562a876764ab2fc7ea1
2yunseong <dbsdltjd3701@naver.com>
2023-05-03 20:05:34 +0900

    feat: 디자인 폰트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Header from './components/Header';
+import TabBar from './components/TabBar';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header title="스톱워치" />
+      <TabBar />
+    </div>
+  );
 }
 
 export default App;
