eacffcd1791f070dd6bc3111053e8e76c7cbfc82
Suyoung <jsy8492@gmail.com>
2023-05-04 16:42:02 +0900

    chore: styled-component 및 타입 정의 설치

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle` 
+  ${reset}
+	* {
+			box-sizing: border-box;
+	}
+
+	body {
+		width: 100vw;
+		height: 100vh;
+
+		font-family: "Roboto", sans-serif;
+  }
+
+	#root {
+		width: 100%;
+		height: 100%;
+	}
+
+  a {
+      text-decoration: none;
+      color: inherit;
+  }
+
+  input, textarea { 
+    -moz-user-select: auto;
+    -webkit-user-select: auto;
+    -ms-user-select: auto;
+    user-select: auto;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  button {
+    border: none;
+    background: none;
+    padding: 0;
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyles;
