dce576465d306e63b80324d79c25926a5692cd3a
2yunseong <dbsdltjd3701@naver.com>
2023-05-08 16:54:43 +0900

    refactor: timer page 마크업 및 스타일링

--- src/pages/Timer/index.tsx
+++ src/pages/Timer/index.tsx
@@ -1,5 +1,31 @@
+import './index.css';
+
 const TimerPage = () => {
-  return <></>;
+  return (
+    <div className="timer-page">
+      <p className="time-display">00:00:00</p>
+      <div className="select-min-container">
+        <button type="button" className="select-min-button">
+          1분
+        </button>
+        <button type="button" className="select-min-button">
+          3분
+        </button>
+        <button type="button" className="select-min-button">
+          10분
+        </button>
+        <button type="button" className="select-min-button">
+          15분
+        </button>
+      </div>
+      <button type="button" className="select-user-button">
+        직접 추가
+      </button>
+      <button type="button" className="start-button">
+        시 작
+      </button>
+    </div>
+  );
 };
 
 export default TimerPage;
