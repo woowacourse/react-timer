b1c819eebe2ac8a8176819e104aa55b85b56ef59
2yunseong <dbsdltjd3701@naver.com>
2023-05-08 17:40:21 +0900

    feat: stop watch 마크업 및 스타일링
        Co-authored-by: 임우찬 <dlaxodud1217@gmail.com>

--- src/pages/StopWatch/index.tsx
+++ src/pages/StopWatch/index.tsx
@@ -1,5 +1,41 @@
+import './index.css';
+
+// TODO : Lap 폰트 칼러는 props로 넘겨주어야함.
+// TODO : table header 간격 조정하기
 const StopWatchPage = () => {
-  return <></>;
+  return (
+    <div className="stop-watch-page">
+      <section className="stop-watch-time-display">
+        <div className="hh-mm-ss">00:00:00</div>
+        <div className="ms">0000</div>
+      </section>
+      <section className="stop-watch-button-container">
+        <button type="button" className="stop-watch-start-button">
+          시작
+        </button>
+        <button type="button" className="stop-watch-lap-button">
+          랩
+        </button>
+      </section>
+      <table className="lap-table">
+        <thead>
+          <tr className="lap-table-header">
+            <th>랩</th>
+            <th>시간</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>
+              <span className="lap-hh-mm-ss">00:11:12</span>
+              <span className="lap-ms">3801</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default StopWatchPage;
