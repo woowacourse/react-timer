00507487e2e9bc2e25db3d71d082692a6a1d659d
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-12 17:06:31 +0900

    feat: 타이머 기능 구현
    Co-authored-by: Nangkyeong Lim <nangkyeonglim@users.noreply.github.com>

--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -2,22 +2,137 @@ import styled from 'styled-components';
 import ControlButton from '../components/common/ControlButton';
 import { ReactComponent as ClockIcon } from '../assets/icons/clock.svg';
 import QuickTimerButton from '../components/common/QuickTimerButton';
+import { useEffect, useState } from 'react';
 
 const TimerPage = () => {
+  const [time, setTime] = useState({ hours: 0, minutes: 0, seconds: 0 });
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer: any;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => {
+          if (
+            prevTime.hours === 0 &&
+            prevTime.minutes === 0 &&
+            prevTime.seconds === 0
+          ) {
+            setIsRunning(false);
+            clearInterval(timer);
+            return prevTime;
+          }
+
+          let seconds = prevTime.seconds;
+          let minutes = prevTime.minutes;
+          let hours = prevTime.hours;
+
+          if (seconds === 0) {
+            if (minutes === 0) {
+              hours -= 1;
+              minutes = 59;
+              seconds = 59;
+            } else {
+              minutes -= 1;
+              seconds = 59;
+            }
+          } else {
+            seconds -= 1;
+          }
+
+          return {
+            hours,
+            minutes,
+            seconds,
+          };
+        });
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handlePause = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime({ hours: 0, minutes: 0, seconds: 0 });
+  };
+
+  const changeTime =
+    (key: string) => (e: React.ChangeEvent<HTMLSelectElement>) => {
+      if (Number(e.target.value) < 0) return;
+      setTime((prev) => ({ ...prev, [key]: Number(e.target.value) }));
+    };
+
   return (
     <>
       <ClockWrapper>
         <ClockIcon />
-        <Clock>00:00:00</Clock>
+        <Clock>
+          {String(time.hours).padStart(2, '0')}:
+          {String(time.minutes).padStart(2, '0')}:
+          {String(time.seconds).padStart(2, '0')}
+        </Clock>
       </ClockWrapper>
       <ButtonWrapper>
-        <QuickTimerButton text='3' onClick={() => {}} />
-        <QuickTimerButton text='5' onClick={() => {}} />
-        <QuickTimerButton text='10' onClick={() => {}} />
+        <QuickTimerButton
+          text='3'
+          onClick={() => setTime({ hours: 0, minutes: 3, seconds: 0 })}
+        />
+        <QuickTimerButton
+          text='5'
+          onClick={() => setTime({ hours: 0, minutes: 5, seconds: 0 })}
+        />
+        <QuickTimerButton
+          text='10'
+          onClick={() => setTime({ hours: 0, minutes: 10, seconds: 0 })}
+        />
       </ButtonWrapper>
+      <InputWrapper>
+        <TimeInput value={time.hours} onChange={changeTime('hours')}>
+          {Array.from({ length: 100 }, (_, index) => (
+            <option key={index} value={index}>
+              {index}
+            </option>
+          ))}
+        </TimeInput>
+        시
+        <TimeInput value={time.minutes} onChange={changeTime('minutes')}>
+          {Array.from({ length: 60 }, (_, index) => (
+            <option key={index} value={index}>
+              {index}
+            </option>
+          ))}
+        </TimeInput>
+        분
+        <TimeInput value={time.seconds} onChange={changeTime('seconds')}>
+          {Array.from({ length: 60 }, (_, index) => (
+            <option key={index} value={index}>
+              {index}
+            </option>
+          ))}
+        </TimeInput>
+        초
+      </InputWrapper>
       <ButtonWrapper>
-        <ControlButton text='취소' onClick={() => {}} />
-        <ControlButton text='시작' onClick={() => {}} />
+        {isRunning ? (
+          <>
+            <ControlButton text='취소' onClick={handleReset} />
+            <ControlButton text='일시정지' onClick={handlePause} />
+          </>
+        ) : (
+          <>
+            <ControlButton text='취소' onClick={handleReset} />
+            <ControlButton text='시작' onClick={handleStart} />
+          </>
+        )}
       </ButtonWrapper>
     </>
   );
@@ -46,8 +161,24 @@ const ButtonWrapper = styled.div`
   justify-content: space-between;
 
   width: 100%;
+`;
 
-  padding-bottom: 24px;
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: 30px 0;
+`;
+
+const TimeInput = styled.select`
+  width: 100px;
+  height: 40px;
+  text-align: center;
+
+  border: 3px solid var(--green-200);
+  border-radius: 12px;
+  color: #525252;
 `;
 
 export default TimerPage;
