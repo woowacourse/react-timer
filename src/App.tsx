fe76c472564605a90c1bfebf4308082396a631e9
Suyoung <jsy8492@gmail.com>
2023-05-12 17:48:27 +0900

    feat: RouterProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+import './App.css';
+import { Outlet } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <header className="">타이머</header>
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,39 @@ import ReactDOM from 'react-dom/client';
 
 import GlobalStyles from './GlobalStyles';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import WorldTimePage from './components/pages/WorldTimePage/WorldTimePage';
+import StopwatchPage from './components/pages/StopwatchPage/StopwatchPage';
+import TimerPage from './components/pages/TimerPage/TimerPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <WorldTimePage />,
+      },
+      {
+        path: 'stopwatch',
+        element: <StopwatchPage />,
+      },
+      {
+        path: 'timer',
+        element: <TimerPage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
