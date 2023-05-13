a9e02f3677ad1e3720beaa2d729171991c78201d
Suyoung <jsy8492@gmail.com>
2023-05-13 16:27:19 +0900

    feat: 시작/재시작 버튼을 포함하는 TriggerButtonSection 컴포넌트 구현

---
+++ src/components/TriggerButtonSection/TriggerButtonSection.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const TriggerButtonSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 130px;
+  column-gap: 50px;
+`;
